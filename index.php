97f7ce716877ecdd04c4dfc6e7df6b9027613ec6
ThomasJones69 <tjones@ziggo.nl>
2017-10-24 21:39:11 +0200

    Aangepast om ook een volle tank te berekenen

--- index.php
+++ index.php
@@ -7,19 +7,31 @@ $prijsperliter = $_GET['prijs'];
 $bedrag = 0;
     if($brandstof == "Diesel"){
         $bedrag = $prijsperliter * 5;
+        $bedragtot = $prijsperliter * 55;
         echo "Prijs per 5 liter ".$brandstof." is ".$bedrag; // wij gaan uit van 5 liter per honderd
+        echo "<br>";
+        echo "Bedrag voor een volle tank van 55 liter is: ".$bedragtot;
     }
     if($brandstof == "Euro"){
         $bedrag = $prijsperliter * 8;
+        $bedragtot = $prijsperliter * 55;
         echo "Prijs per 8 liter ".$brandstof." is ".$bedrag; // wij gaan uit van 8 liter per honderd
+        echo "<br";
+        echo "Bedrag voor een volle tank van 55 liter is: ".$bedragtot;
     }
     if($brandstof == "Super"){
         $bedrag = $prijsperliter * 11;
+        $bedragtot = $prijsperliter * 55;
         echo "Prijs per 11 liter ".$brandstof." is ".$bedrag; // wij gaan uit van 11 liter per honderd
+        echo "<br>";
+        echo "Bedrag voor een volle tank van 55 liter is: ".$bedragtot;
     }    
     if($brandstof == "LPG"){
         $bedrag = $prijsperliter * 15;
+        $bedragtot = $prijsperliter * 55;
         echo "Prijs per 15 liter ".$brandstof." is ".$bedrag; // wij gaan uit van 15 liter per honderd
+        echo"<br>";
+        echo "Bedrag voor een volle tank van 55 liter is: ".$bedragtot;
     }
 
 ?>
