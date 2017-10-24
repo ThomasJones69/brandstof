a58a926d279227abad281667cad50510740c6a2f
ThomasJones69 <tjones@ziggo.nl>
2017-10-24 16:54:36 +0200

    De eerste cmmmmommit voor Diesel

--- index.php
+++ index.php
@@ -7,7 +7,19 @@ $prijsperliter = $_GET['prijs'];
 $bedrag = 0;
     if($brandstof == "Diesel"){
         $bedrag = $prijsperliter * 5;
-        echo "Prijs per 5 liter is: ".$bedrag; // wij gaan uit van 5 liter per honderd
+        echo "Prijs per 5 liter ".$brandstof." is ".$bedrag; // wij gaan uit van 5 liter per honderd
+    }
+    if($brandstof == "Euro"){
+        $bedrag = $prijsperliter * 8;
+        echo "Prijs per 8 liter ".$brandstof." is ".$bedrag; // wij gaan uit van 8 liter per honderd
+    }
+    if($brandstof == "Super"){
+        $bedrag = $prijsperliter * 11;
+        echo "Prijs per 11 liter ".$brandstof." is ".$bedrag; // wij gaan uit van 11 liter per honderd
+    }    
+    if($brandstof == "LPG"){
+        $bedrag = $prijsperliter * 15;
+        echo "Prijs per 15 liter ".$brandstof." is ".$bedrag; // wij gaan uit van 15 liter per honderd
     }
 
 ?>
